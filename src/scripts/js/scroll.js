80b09eacda1d03562f9b373f07d2452cb36e265f
sayajuli <sahruljulistia1@gmail.com>
2024-04-18 20:01:33 +0700

    tinggal navbar

--- src/scripts/js/scroll.js
+++ src/scripts/js/scroll.js
@@ -1,4 +1,8 @@
 const rootElement = document.querySelector(":root");
+const audioIconWrapper = document.querySelector('.audio-icon-wrapper');
+const audioIcon = document.querySelector('.audio-icon-wrapper i')
+const bgm = document.querySelector('#bgm');
+let isPlaying = false;
 
 function disableScroll(){
     scrollTop = window.pageYOffset || document.documentElement.scrollTop;
@@ -14,9 +18,33 @@ function disableScroll(){
 function enableScroll(){
     window.onscroll = function(){}
     rootElement.style.scrollBehavior = 'smooth';
-    localStorage.setItem('opened', 'true');
+    // localStorage.setItem('opened', 'true');
+    playAudio()
 }
 
-if (!localStorage.getItem('opened')){
-    disableScroll();
+function playAudio() {
+    bgm.volume = 0.1;
+    audioIconWrapper.style.display = 'flex';
+    bgm.play();
+    isPlaying = true;
 }
+
+audioIconWrapper.onclick = function() {
+    if (isPlaying) {
+        bgm.pause();
+        audioIcon.classList.remove('bi-disc');
+        audioIcon.classList.add('bi-pause-circle');
+    } else {
+        bgm.play();
+        audioIcon.classList.add('bi-disc');
+        audioIcon.classList.remove('bi-pause-circle');
+    }
+
+    isPlaying = !isPlaying;
+}
+
+// if (!localStorage.getItem('opened')){
+//     disableScroll();
+// }
+
+disableScroll();
